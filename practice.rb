ec993fcec319518ee681690857bd2ae5d10740cf
Mohammed shaikh <mqshaikh8@gmail.com>
2019-11-11 14:19:28 +0000

    Automatically backed up by Learn

---
+++ practice.rb
@@ -0,0 +1,9 @@
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
+puts spice_rack
+spice_rack[1][100] = "Poodle Dinner"
+spice_rack[1][1] = "Extract of Pizza"
+puts spice_rack
---
+++ ruby_learn_Conditionals.rb
@@ -0,0 +1,28 @@
+# name_one = "Qasim"
+
+# if name_one == "Qasim"
+#   puts "Qasim is awesome"
+# else name_one == "Hanzala"
+#   puts "Hanzala sucks"
+# end
+
+
+# name = "Owais"
+
+# puts "Q"
+# case name
+#   when "Qasim"
+#     puts "Qasim is the best"
+#   when "Owais"
+#     puts "Abba is deep"
+#   when "Hanzala"
+#     puts "Hanzala is stupid"
+# end
+# puts "Shutup"
+n = 
+count = 0 
+while count <= n do
+  puts "I ran #{count} times"
+  count = count + 1
+  n = n + 2
+end
